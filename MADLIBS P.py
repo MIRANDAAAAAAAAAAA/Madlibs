d12d35e599d817d7ec0cf874443556801e90a4a7
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-29 13:16:22 -0700

    Create MADLIBS P.py

---
+++ MADLIBS P.py
@@ -0,0 +1,24 @@
+print("Did you know I have a pet - Fast - ?")
+print("- Decades - likes to run around the school and the after he went to visit - -.")
+print("Silly - is a big fan of a kpop grroup called SEVENTEN and her bias is - London - he is - 16 - old.")
+print("One night he went to the store and he bought - Phones - for him and his firends to eat after they got home from school he bought- Pecils -, - Telsa -, - Ships - .")
+print("- Miranda - went to the mall and she found her firend from high school and started to talk to her about he life in - The middl eof the atlatic ocean -.")
+print("Abraham Lincoln was the - 6th - president and he was caught in - Korea - with his mom who had just died.")
+print("Do you think that he was going to buy - headphones - or was he there to - jump - he had brought with him his - hamster - and so  much money.")
+print("Ab was buying so many - Feet - and then he went all - To ball his eyes out - and started yeling - I ate my cat - to the people.")
+print("Chis was the - goat - one to - fart - during the - olypics - of his cousin who he had a - crush - on and then he decide to - Jump  - her coffin after he - - .")
+print("Motiaa takes - Everything - seriously and falls for - Everyhting - and then - wines to - me a - - liar.")
+print("Tomorrow is - Natinol dumbasss DAY  - and maria was going to the- coffe shop - and then she - burn her self  - and was  - ugly - by - herself -.")
+print("My favorit car is the - very expensive - and - gorgious - will get it - not soon - day  becuase I - am broke - in - the head -.")
+print("My dream place to live in is - Asia - and I want to live there because there are - many hot men - many - nice - places to go ther and the people there are so - hot - and also I hvae always wnated to go to the - mall - there - maybe-  the temple are like that in the historical - pyrimids - I watch and obseses about - men - day.")    
+NEW ONES:
+print("-Switzzeerland - is a places I realy want to go to because they have - balls - - Dukkie - there and I want to try there - -")
+print("- WINWIN - did an advetisment for a -Poland - brand and then he because the ambassidre for that - -.")
+print("I was - - the MIGHAO left the - - show but I will see him in a - - variety - - and it is a dating - -.")
+print("My - - song from WONDER - - is TELL - - and I am - - to it now")
+noun1 = input("Choose a noun")
+
+
+
+
+
